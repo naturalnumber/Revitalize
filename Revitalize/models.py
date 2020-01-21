921fc7e4084889259146735ff4461379a1817a47
Allan Stewart <naturalnumber@gmail.com>
2020-01-21 14:51:26 -0400

    initial template fix

--- Revitalize/models.py
+++ Revitalize/models.py
@@ -10,6 +10,7 @@ class ModelBase(models.Model):
     rm_fields_serialize: list = rm_fields.copy()
 
     id = models.AutoField(editable=False, primary_key=True)
+
     flags = models.TextField(blank=False, help_text="This should be a JSON.", default="{}")
 
     creation_time = models.DateTimeField(auto_now_add=True, null=False)
@@ -20,20 +21,21 @@ class ModelBase(models.Model):
 
 
 class ModelMore(ModelBase):
-    rm_fields: list = ModelBase.rm_fields.copy()
-    rm_fields_display: list = ModelBase.rm_fields.copy()
-    rm_fields_filter: list = ModelBase.rm_fields.copy()
-    rm_fields_search: list = ModelBase.rm_fields.copy()
-    rm_fields_serialize: list = ModelBase.rm_fields.copy()
+    rm_fields: list = ['name', 'description', 'notes']
+    rm_fields_display: list = ['name']
+    rm_fields_filter: list = ['name']
+    rm_fields_search: list = ['name']
+    rm_fields_serialize: list = ['name', 'description', 'notes']
 
-    rm_fields.extend(['name', 'description', 'notes'])
-    rm_fields_display.extend(['name'])
-    rm_fields_filter.extend(['name'])
-    rm_fields_search.extend(['name'])
-    rm_fields_serialize.extend(['name', 'description', 'notes'])
+    rm_fields.extend(ModelBase.rm_fields)
+    rm_fields_display.extend(ModelBase.rm_fields_display)
+    rm_fields_filter.extend(ModelBase.rm_fields_filter)
+    rm_fields_search.extend(ModelBase.rm_fields_search)
+    rm_fields_serialize.extend(ModelBase.rm_fields_serialize)
 
     name = models.CharField(max_length=40, blank=False, unique=True)
     description = models.TextField(blank=True, null=False)
+
     notes = models.TextField(blank=True, null=False)
 
     class Meta:
@@ -44,14 +46,17 @@ class ModelMore(ModelBase):
 
 
 class ModelProcessable(ModelMore):
-    rm_fields: list = ModelMore.rm_fields.copy()
-    rm_fields_display: list = ModelMore.rm_fields.copy()
-    rm_fields_filter: list = ModelMore.rm_fields.copy()
-    rm_fields_search: list = ModelMore.rm_fields.copy()
-    rm_fields_serialize: list = ModelMore.rm_fields.copy()
+    rm_fields: list = ['specification', 'analysis']
+    rm_fields_display: list = []
+    rm_fields_filter: list = []
+    rm_fields_search: list = []
+    rm_fields_serialize: list = ['specification', 'analysis']
 
-    rm_fields.extend(['specification', 'analysis'])
-    rm_fields_serialize.extend(['specification', 'analysis'])
+    rm_fields.extend(ModelMore.rm_fields)
+    rm_fields_display.extend(ModelMore.rm_fields_display)
+    rm_fields_filter.extend(ModelMore.rm_fields_filter)
+    rm_fields_search.extend(ModelMore.rm_fields_search)
+    rm_fields_serialize.extend(ModelMore.rm_fields_serialize)
 
     specification = models.TextField(blank=False, help_text="This should be a JSON.")
     analysis = models.TextField(blank=False, help_text="This should be a JSON.")
@@ -61,17 +66,17 @@ class ModelProcessable(ModelMore):
 
 
 class AbstractIndicator(ModelProcessable):
-    rm_fields: list = ModelProcessable.rm_fields.copy()
-    rm_fields_display: list = ModelProcessable.rm_fields.copy()
-    rm_fields_filter: list = ModelProcessable.rm_fields.copy()
-    rm_fields_search: list = ModelProcessable.rm_fields.copy()
-    rm_fields_serialize: list = ModelProcessable.rm_fields.copy()
+    rm_fields: list = ['type']
+    rm_fields_display: list = ['type']
+    rm_fields_filter: list = ['type']
+    rm_fields_search: list = ['type']
+    rm_fields_serialize: list = ['type']
 
-    rm_fields.extend(['type'])
-    rm_fields_display.extend(['type'])
-    rm_fields_filter.extend(['type'])
-    rm_fields_search.extend(['type'])
-    rm_fields_serialize.extend(['type'])
+    rm_fields.extend(ModelProcessable.rm_fields)
+    rm_fields_display.extend(ModelProcessable.rm_fields_display)
+    rm_fields_filter.extend(ModelProcessable.rm_fields_filter)
+    rm_fields_search.extend(ModelProcessable.rm_fields_search)
+    rm_fields_serialize.extend(ModelProcessable.rm_fields_serialize)
 
     class DataType(models.TextChoices):
         UNKNOWN = '?', _('Unknown')
@@ -88,17 +93,17 @@ class AbstractIndicator(ModelProcessable):
 
 
 class AbstractDataPoint(ModelBase):
-    rm_fields: list = ModelBase.rm_fields.copy()
-    rm_fields_display: list = ModelBase.rm_fields.copy()
-    rm_fields_filter: list = ModelBase.rm_fields.copy()
-    rm_fields_search: list = ModelBase.rm_fields.copy()
-    rm_fields_serialize: list = ModelBase.rm_fields.copy()
+    rm_fields: list = ['time', 'value', 'notes']
+    rm_fields_display: list = ['time', 'value']
+    rm_fields_filter: list = ['time', 'value']
+    rm_fields_search: list = ['time', 'value']
+    rm_fields_serialize: list = ['time', 'description', 'notes']
 
-    rm_fields.extend(['time', 'value', 'notes'])
-    rm_fields_display.extend(['time', 'value'])
-    rm_fields_filter.extend(['time', 'value'])
-    rm_fields_search.extend(['time', 'value'])
-    rm_fields_serialize.extend(['time', 'description', 'notes'])
+    rm_fields.extend(ModelBase.rm_fields)
+    rm_fields_display.extend(ModelBase.rm_fields_display)
+    rm_fields_filter.extend(ModelBase.rm_fields_filter)
+    rm_fields_search.extend(ModelBase.rm_fields_search)
+    rm_fields_serialize.extend(ModelBase.rm_fields_serialize)
 
     time = models.DateTimeField(null=False)
     value = models.TextField(blank=False, help_text="This should be a JSON.", default="{}")
@@ -110,29 +115,29 @@ class AbstractDataPoint(ModelBase):
 
 
 class Client(ModelBase):
-    rm_fields: list = ModelBase.rm_fields.copy()
-    rm_fields_display: list = ModelBase.rm_fields.copy()
-    rm_fields_filter: list = ModelBase.rm_fields.copy()
-    rm_fields_search: list = ModelBase.rm_fields.copy()
-    #rm_fields_serialize: list = ModelBase.rm_fields.copy()
-
-    rm_fields.extend(['username', 'first_name', 'middle_name', 'last_name', 'birth_date', 'gender',
-                      'phone_number', 'phone_number_alt', 'email',
-                      'street_address', 'city', 'province', 'country', 'postal_code',
-                      'ec_first_name', 'ec_middle_name', 'ec_last_name', 'ec_phone_number',
-                      'physician', 'points', 'password_flag', 'preferences', 'notes'])
-    rm_fields_display.extend(['username', 'first_name', 'middle_name', 'last_name', 'birth_date', 'gender',
+    rm_fields: list = ['username', 'first_name', 'middle_name', 'last_name', 'birth_date', 'gender',
+                       'phone_number', 'phone_number_alt', 'email',
+                       'street_address', 'city', 'province', 'country', 'postal_code',
+                       'ec_first_name', 'ec_middle_name', 'ec_last_name', 'ec_phone_number',
+                       'physician', 'points', 'password_flag', 'preferences', 'notes']
+    rm_fields_display: list = ['username', 'first_name', 'middle_name', 'last_name', 'birth_date', 'gender',
+                               'phone_number', 'phone_number_alt', 'email',
+                               'street_address', 'city', 'province', 'country', 'postal_code',
+                               'physician', 'points']
+    rm_fields_filter: list = ['username', 'first_name', 'last_name', 'birth_date', 'gender',
+                              'city', 'province', 'country', 'postal_code',
+                              'physician', 'points']
+    rm_fields_search: list = ['username', 'first_name', 'last_name', 'birth_date', 'gender',
                               'phone_number', 'phone_number_alt', 'email',
                               'street_address', 'city', 'province', 'country', 'postal_code',
-                              'physician', 'points'])
-    rm_fields_filter.extend(['username', 'first_name', 'last_name', 'birth_date', 'gender',
-                             'city', 'province', 'country', 'postal_code',
-                             'physician', 'points'])
-    rm_fields_search.extend(['username', 'first_name', 'last_name', 'birth_date', 'gender',
-                             'phone_number', 'phone_number_alt', 'email',
-                             'street_address', 'city', 'province', 'country', 'postal_code',
-                             'physician', 'points'])
+                              'physician', 'points']
     rm_fields_serialize = rm_fields.copy()
+
+    rm_fields.extend(ModelBase.rm_fields)
+    rm_fields_display.extend(ModelBase.rm_fields_display)
+    rm_fields_filter.extend(ModelBase.rm_fields_filter)
+    rm_fields_search.extend(ModelBase.rm_fields_search)
+    rm_fields_serialize.extend(ModelBase.rm_fields_serialize)
 
     class GenderType(models.TextChoices):
         Male = 'M', _('Male')
@@ -150,13 +155,13 @@ class Client(ModelBase):
 
     phone_number = models.CharField(max_length=40, blank=False)
     phone_number_alt = models.CharField(max_length=40, null=False, blank=True)
-    email = models.EmailField(blank=False, help_text="The email address for a user.") #, unique=True
+    email = models.EmailField(blank=False, help_text="The email address for a user.")  # , unique=True
 
     street_address = models.CharField(max_length=100, blank=False)
     city = models.CharField(max_length=100, blank=False)
     province = models.CharField(max_length=100, blank=False)
     country = models.CharField(max_length=100, blank=False)
-    postal_code = models.CharField(max_length=10, blank=False) # Abstract into an address object?
+    postal_code = models.CharField(max_length=10, blank=False)  # Abstract into an address object?
 
     ec_first_name = models.CharField(max_length=40, null=False, blank=True)
     ec_middle_name = models.CharField(max_length=40, null=False, blank=True)
@@ -164,8 +169,8 @@ class Client(ModelBase):
     ec_phone_number = models.CharField(max_length=40, null=False, blank=True)
 
     physician = models.CharField(max_length=40, null=False, blank=True)
-    points = models.BigIntegerField(null=False, verbose_name="Health Currency", default=0)  # Int or Decimal?
 
+    points = models.BigIntegerField(null=False, verbose_name="Health Currency", default=0)  # Int or Decimal?
     personal_message = models.CharField(max_length=280, blank=True, null=False)
     picture = models.ImageField(upload_to='profile_pictures/', blank=True)
 
@@ -181,19 +186,20 @@ class Client(ModelBase):
         else:
             return f"{self.first_name} {self.last_name}"
 
+
 class Cohort(ModelMore):
-    rm_fields: list = ModelMore.rm_fields.copy()
-    rm_fields_display: list = ModelMore.rm_fields.copy()
-    rm_fields_filter: list = ModelMore.rm_fields.copy()
-    rm_fields_search: list = ModelMore.rm_fields.copy()
-    rm_fields_serialize: list = ModelMore.rm_fields.copy()
+    rm_fields: list = ['type', 'data']
+    rm_fields_display: list = ['type']
+    rm_fields_filter: list = ['type']
+    rm_fields_search: list = ['type']
+    rm_fields_serialize: list = ['type', 'data']
     rm_fields_foreign: list = ['clients']
 
-    rm_fields.extend(['type', 'data'])
-    rm_fields_display.extend(['type'])
-    rm_fields_filter.extend(['type'])
-    rm_fields_search.extend(['type'])
-    rm_fields_serialize.extend(['type', 'data'])
+    rm_fields.extend(ModelMore.rm_fields)
+    rm_fields_display.extend(ModelMore.rm_fields_display)
+    rm_fields_filter.extend(ModelMore.rm_fields_filter)
+    rm_fields_search.extend(ModelMore.rm_fields_search)
+    rm_fields_serialize.extend(ModelMore.rm_fields_serialize)
 
     class CohortType(models.TextChoices):
         Male = 'PLM', _('Pulminary')
@@ -207,17 +213,17 @@ class Cohort(ModelMore):
 
 
 class Form(ModelProcessable):
-    rm_fields: list = ModelProcessable.rm_fields.copy()
-    rm_fields_display: list = ModelProcessable.rm_fields.copy()
-    rm_fields_filter: list = ModelProcessable.rm_fields.copy()
-    rm_fields_search: list = ModelProcessable.rm_fields.copy()
-    rm_fields_serialize: list = ModelProcessable.rm_fields.copy()
+    rm_fields: list = ['type', 'display']
+    rm_fields_display: list = ['type']
+    rm_fields_filter: list = ['type']
+    rm_fields_search: list = ['type']
+    rm_fields_serialize: list = ['type', 'display']
 
-    rm_fields.extend(['type', 'display'])
-    rm_fields_display.extend(['type'])
-    rm_fields_filter.extend(['type'])
-    rm_fields_search.extend(['type'])
-    rm_fields_serialize.extend(['type', 'display'])
+    rm_fields.extend(ModelProcessable.rm_fields)
+    rm_fields_display.extend(ModelProcessable.rm_fields_display)
+    rm_fields_filter.extend(ModelProcessable.rm_fields_filter)
+    rm_fields_search.extend(ModelProcessable.rm_fields_search)
+    rm_fields_serialize.extend(ModelProcessable.rm_fields_serialize)
 
     class FormType(models.TextChoices):
         UNKNOWN = '?', _('Unknown')
@@ -231,18 +237,18 @@ class Form(ModelProcessable):
 
 
 class Submission(ModelBase):
-    rm_fields: list = ModelBase.rm_fields.copy()
-    rm_fields_display: list = ModelBase.rm_fields.copy()
-    rm_fields_filter: list = ModelBase.rm_fields.copy()
-    rm_fields_search: list = ModelBase.rm_fields.copy()
-    rm_fields_serialize: list = ModelBase.rm_fields.copy()
+    rm_fields: list = ['time', 'values', 'notes']
+    rm_fields_display: list = ['time']
+    rm_fields_filter: list = ['time']
+    rm_fields_search: list = ['time']
+    rm_fields_serialize: list = ['time', 'values', 'notes']
     rm_fields_foreign: list = ['client', 'form']
 
-    rm_fields.extend(['time', 'values', 'notes'])
-    rm_fields_display.extend(['time'])
-    rm_fields_filter.extend(['time'])
-    rm_fields_search.extend(['time'])
-    rm_fields_serialize.extend(['time', 'values', 'notes'])
+    rm_fields.extend(ModelBase.rm_fields)
+    rm_fields_display.extend(ModelBase.rm_fields_display)
+    rm_fields_filter.extend(ModelBase.rm_fields_filter)
+    rm_fields_search.extend(ModelBase.rm_fields_search)
+    rm_fields_serialize.extend(ModelBase.rm_fields_serialize)
 
     client = models.ForeignKey('Client', on_delete=models.CASCADE, null=False)  # related_name='+'
     form = models.ForeignKey('Form', on_delete=models.SET_NULL, null=True)
@@ -257,20 +263,32 @@ class Submission(ModelBase):
 
 
 class Indicator(AbstractIndicator):
-    rm_fields: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_display: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_filter: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_search: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_serialize: list = AbstractIndicator.rm_fields.copy()
+    rm_fields: list = []
+    rm_fields_display: list = []
+    rm_fields_filter: list = []
+    rm_fields_search: list = []
+    rm_fields_serialize: list = []
+
+    rm_fields.extend(AbstractIndicator.rm_fields)
+    rm_fields_display.extend(AbstractIndicator.rm_fields)
+    rm_fields_filter.extend(AbstractIndicator.rm_fields)
+    rm_fields_search.extend(AbstractIndicator.rm_fields)
+    rm_fields_serialize.extend(AbstractIndicator.rm_fields)
 
 
 class DataPoint(AbstractDataPoint):
-    rm_fields: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_display: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_filter: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_search: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_serialize: list = AbstractDataPoint.rm_fields.copy()
+    rm_fields: list = []
+    rm_fields_display: list = []
+    rm_fields_filter: list = []
+    rm_fields_search: list = []
+    rm_fields_serialize: list = []
     rm_fields_foreign: list = ['client', 'indicator']
+
+    rm_fields.extend(AbstractDataPoint.rm_fields)
+    rm_fields_display.extend(AbstractDataPoint.rm_fields)
+    rm_fields_filter.extend(AbstractDataPoint.rm_fields)
+    rm_fields_search.extend(AbstractDataPoint.rm_fields)
+    rm_fields_serialize.extend(AbstractDataPoint.rm_fields)
 
     client = models.ForeignKey('Client', on_delete=models.CASCADE, null=False)  # related_name='+'
     indicator = models.ForeignKey('Indicator', on_delete=models.SET_NULL, null=True)
@@ -280,18 +298,18 @@ class DataPoint(AbstractDataPoint):
 
 
 class Goal(ModelBase):
-    rm_fields: list = ModelBase.rm_fields.copy()
-    rm_fields_display: list = ModelBase.rm_fields.copy()
-    rm_fields_filter: list = ModelBase.rm_fields.copy()
-    rm_fields_search: list = ModelBase.rm_fields.copy()
-    rm_fields_serialize: list = ModelBase.rm_fields.copy()
+    rm_fields: list = ['start', 'target', 'completed', 'notes']
+    rm_fields_display: list = ['start', 'completed']
+    rm_fields_filter: list = ['start', 'completed']
+    rm_fields_search: list = ['start', 'completed']
+    rm_fields_serialize: list = ['start', 'target', 'completed', 'notes']
     rm_fields_foreign: list = ['client', 'indicator']
 
-    rm_fields.extend(['start', 'target', 'completed', 'notes'])
-    rm_fields_display.extend(['start', 'completed'])
-    rm_fields_filter.extend(['start', 'completed'])
-    rm_fields_search.extend(['start', 'completed'])
-    rm_fields_serialize.extend(['start', 'target', 'completed', 'notes'])
+    rm_fields.extend(ModelBase.rm_fields)
+    rm_fields_display.extend(ModelBase.rm_fields)
+    rm_fields_filter.extend(ModelBase.rm_fields)
+    rm_fields_search.extend(ModelBase.rm_fields)
+    rm_fields_serialize.extend(ModelBase.rm_fields)
 
     start = models.DateTimeField(null=False)
     target = models.TextField(blank=False, help_text="This should be a JSON.")
@@ -308,31 +326,36 @@ class Goal(ModelBase):
 
 
 class AnonymizedIndicator(AbstractIndicator):
-    rm_fields: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_display: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_filter: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_search: list = AbstractIndicator.rm_fields.copy()
-    rm_fields_serialize: list = AbstractIndicator.rm_fields.copy()
+    rm_fields: list = ['anonmization']
+    rm_fields_display: list = []
+    rm_fields_filter: list = []
+    rm_fields_search: list = []
+    rm_fields_serialize: list = ['anonmization']
     rm_fields_foreign: list = ['indicator']
 
-    rm_fields.extend(['anonmization'])
-    rm_fields_serialize.extend(['anonmization'])
+    rm_fields.extend(AbstractIndicator.rm_fields)
+    rm_fields_display.extend(AbstractIndicator.rm_fields)
+    rm_fields_filter.extend(AbstractIndicator.rm_fields)
+    rm_fields_search.extend(AbstractIndicator.rm_fields)
+    rm_fields_serialize.extend(AbstractIndicator.rm_fields)
 
     indicator = models.ForeignKey('Indicator', on_delete=models.SET_NULL, null=True)
-
     anonmization = models.TextField(blank=False, help_text="This should be a JSON.")
 
 
 class CohortDataPoint(AbstractDataPoint):
-    rm_fields: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_display: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_filter: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_search: list = AbstractDataPoint.rm_fields.copy()
-    rm_fields_serialize: list = AbstractDataPoint.rm_fields.copy()
+    rm_fields: list = ['a_client_id']
+    rm_fields_display: list = []
+    rm_fields_filter: list = []
+    rm_fields_search: list = []
+    rm_fields_serialize: list = ['a_client_id']
     rm_fields_foreign: list = ['cohort', 'a_indicator']
 
-    rm_fields.extend(['a_client_id'])
-    rm_fields_serialize.extend(['a_client_id'])
+    rm_fields.extend(AbstractDataPoint.rm_fields)
+    rm_fields_display.extend(AbstractDataPoint.rm_fields)
+    rm_fields_filter.extend(AbstractDataPoint.rm_fields)
+    rm_fields_search.extend(AbstractDataPoint.rm_fields)
+    rm_fields_serialize.extend(AbstractDataPoint.rm_fields)
 
     cohort = models.ForeignKey('Cohort', on_delete=models.CASCADE, null=False)  # related_name='+'
     a_indicator = models.ForeignKey('AnonymizedIndicator', on_delete=models.SET_NULL, null=True)
