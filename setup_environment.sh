1075374b024f051e55dbb2df0a894d1505f58be6
Allan Stewart <naturalnumber@gmail.com>
2020-01-29 14:40:48 -0400

    Minor script file fix

--- setup_environment.sh
+++ setup_environment.sh
@@ -6,13 +6,28 @@ set -o pipefail
 
 echo "Welcome to the Revitalize Virtual Environment Setup"
 echo "Your operating system has been determined to be: acceptable :)"
-read -p "Would you like to initialize the virtual environment in this directory? Y/n" -n 1 -r
+read -p "Would you like to initialize the virtual environment in this directory? Y/n " -n 1 -r
 echo
 
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]
 then
-    python3.7 -m venv venv
-    source venv/bin/activate
-    python3.7 -m pip install -r requirements.txt
+    read -p "Do you have more than one version of Python 3? Y/n " -n 1 -r
+    echo
+
+    if [[ ${REPLY} =~ ^[Yy]$ ]]
+    then
+
+        python3.7 -m venv venv
+        source venv/bin/activate
+        python3.7 -m pip install -r requirements.txt
+
+    else
+
+        python3 -m venv venv
+        source venv/bin/activate
+        python3 -m pip install -r requirements.txt
+
+    fi
+
 fi
