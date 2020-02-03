77c27ba9cf34a0a823657d56f00cc747dd599c75
Matthew Bowlan <mbowlan@upei.ca>
2020-02-03 14:42:39 -0400

    simplejwt installed

--- setup_environment.sh
+++ setup_environment.sh
@@ -12,7 +12,7 @@ echo
 
 if [[ ${REPLY} =~ ^[Yy]$ ]]
 then
-    python3.7 -m venv venv
+    python3 -m venv venv
     source venv/bin/activate
-    python3.7 -m pip install -r requirements.txt
+    python3 -m pip install -r requirements.txt
 fi
