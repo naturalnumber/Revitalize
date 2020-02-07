6616e97bdbf0d03811d4729297265c16390600ba
Matthew Bowlan <mbowlan@upei.ca>
2020-02-07 17:41:31 -0400

    simplejwt settings added access token lifetime changed to 30 min from 5 min in an attempt to improve performance

--- backend_api/settings.py
+++ backend_api/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from datetime import timedelta
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -131,3 +133,13 @@ CORS_ORIGIN_WHITELIST = [
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+# Authentication: simplejwt
+# https://github.com/davesque/django-rest-framework-simplejwt
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
