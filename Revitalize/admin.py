24bba6873748f415cc3c597068867d14426966f1
Matthew Bowlan <mbowlan@upei.ca>
2020-03-21 13:12:15 -0300

    Added an admin page for lab tech - currently shows how models can only be registered for one site.

--- Revitalize/admin.py
+++ Revitalize/admin.py
@@ -1,31 +1,48 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 from Revitalize.models import *
 
-admin.site.register(Text)
-admin.site.register(String)
-admin.site.register(StringGroup)
-admin.site.register(Address)
-admin.site.register(CanadianAddress)
-admin.site.register(Profile)
-admin.site.register(Form)
-admin.site.register(Survey)
-admin.site.register(MedicalLab)
-admin.site.register(TextElement)
-admin.site.register(QuestionGroup)
-admin.site.register(Question)
-admin.site.register(TextQuestion)
-admin.site.register(IntQuestion)
-admin.site.register(FloatQuestion)
-admin.site.register(IntRangeQuestion)
-admin.site.register(FloatRangeQuestion)
-admin.site.register(BooleanChoiceQuestion)
-admin.site.register(ExclusiveChoiceQuestion)
-admin.site.register(MultiChoiceQuestion)
-admin.site.register(Submission)
-admin.site.register(TextResponse)
-admin.site.register(IntResponse)
-admin.site.register(FloatResponse)
-admin.site.register(Indicator)
-admin.site.register(IntDataPoint)
-admin.site.register(FloatDataPoint)
+
+class AdminsSite(AdminSite):
+    site_header = 'Admin Administration'
+
+
+admin_site = AdminsSite(name='admin_site')
+
+admin_site.register(Text)
+admin_site.register(String)
+admin_site.register(StringGroup)
+admin_site.register(Address)
+admin_site.register(CanadianAddress)
+# admin_site.register(Profile)
+admin_site.register(Form)
+admin_site.register(Survey)
+admin_site.register(MedicalLab)
+admin_site.register(TextElement)
+admin_site.register(QuestionGroup)
+admin_site.register(Question)
+admin_site.register(TextQuestion)
+admin_site.register(IntQuestion)
+admin_site.register(FloatQuestion)
+admin_site.register(IntRangeQuestion)
+admin_site.register(FloatRangeQuestion)
+admin_site.register(BooleanChoiceQuestion)
+admin_site.register(ExclusiveChoiceQuestion)
+admin_site.register(MultiChoiceQuestion)
+admin_site.register(Submission)
+admin_site.register(TextResponse)
+admin_site.register(IntResponse)
+admin_site.register(FloatResponse)
+admin_site.register(Indicator)
+admin_site.register(IntDataPoint)
+admin_site.register(FloatDataPoint)
+
+
+class LabTechSite(AdminSite):
+    site_header = 'Lab Tech Administration'
+
+
+lab_tech_site = LabTechSite(name='labtech_site')
+
+
+lab_tech_site.register(Profile)
--- backend_api/urls.py
+++ backend_api/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from Revitalize.admin import admin_site, lab_tech_site
 from django.urls import include, path
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
@@ -23,7 +24,8 @@ from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
+        path('admin/', admin_site.urls),
+        path('labtech/', lab_tech_site.urls),
         path('api/', include('Revitalize.urls')),
         # TODO could these be put in the Revitalize urls.py?
         # Authentication : https://github.com/davesque/django-rest-framework-simplejwt
