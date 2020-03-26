6b39e535d78cf0013d3368a6030216c41f874ad7
Allan Stewart <naturalnumber@gmail.com>
2020-03-26 00:40:43 -0300

    typo and documentation

--- Revitalize/urls.py
+++ Revitalize/urls.py
@@ -17,22 +17,45 @@ router = routers.DefaultRouter()
 # GET -> all lab values for that id
 # POST -> min_date max_date to filter
 
+
 _new_patterns = []
 
+#  apiCall('/profiles/user/', {method: 'GET'}) // get user profile
+# profile/user/ -> retrieve the profile for a single user
 router.register('profiles', ProfileRetrievalViewSet, basename='profile')
 
-router.register('lab-values', LabValueRetrievalViewSet, basename='lab-value')
+#  apiCall('/lab-values/user/', { method: 'GET' }) // get lab value history (should this be /indicators/lab-value/user/?
+# lab-value/user/ -> retrieve all lab values associated with a user in format: [{name: 'Weight", value: 120, unit: 'lb', submission_date: 32412341234}, {name: 'Weight', value: 125', unit: 'lb', submission_date:  3424324}]
+# lab-value/id/user/ -> retrieve all lab values of a certain id for a user in format: [{name: 'Weight", value: 120, submission_date: 32412341234}, {name: 'Weight', value: 125', submission_date:  3424324}]
+# 	GET -> all lab values associated with an id and user (history)
+# 	POST -> {min_date, max_date} filtered version
+router.register('lab-values', LabValueRetrievalViewSet, basename='lab-value') # LabValueRetrievalViewSet
 
-router.register('survey-values', SurveyValueRetrievalViewSet, basename='survey-value')
+router.register('surveys', SurveyValueRetrievalViewSet, basename='survey-value') # SurveyValueRetrievalViewSet
 
 router.register('indicators', IndicatorRetrievalViewSet, basename='indicator')
 
+#  apiCall('/indicators/survey/user/', { method: 'GET' }) // get all survey indicators for user (to select which one to graph)
+#  apiCall(`/indicators/survey/${change.selector}/user/`, { method: 'POST', body: range }) // get graph data for survey indicators
+# indicator/survey/user/ -> retrieves survey indicators associated with a user
+# 	GET -> any submission_date/no submission_date
+# 	POST -> {min_date, max_date} filtered version
 router.register('indicators/survey', SurveyIndicatorRetrievalViewSet, basename='indicator-survey')
 
-router.register('indicators/lab-value', LabIndicatorRetrievalViewSet, basename='indicator-lab-value')
+#  apiCall(`/indicators/lab-value/${change.selector}/user/`, { method: 'POST', body: range }) // get graph data for lab value
+# indicator/lab-value/user/ -> retrieves lab value indicators associated with a user
+# 	GET -> a submission_date/no submission_date
+# 	POST -> {min_date, max_date} filtered version
+# indicator/lab-value/id/user/recent -> most recent submission
+router.register('indicators/lab-value', LabIndicatorRetrievalViewSet, basename='indicator-lab-value') # LabIndicatorRetrievalViewSet
 
-router.register('surveys/user', UserSurveyHistoryViewSet, basename='survey')
+#  apiCall('/surveys/user/', { method: 'GET'}) // get submitted survey history
+#  apiCall('/surveys/user/available/', { method: 'GET'}) // get surveys avaialble to complete
+#  apiCall(`/surveys/${surveyId}/submit/`, { method: 'POST', body: model }, false) // submit survey
+# surveys/user/ -> retrieve all submitted surveys associated with a user (same as survey history endpoint)
+router.register('surveys/user', UserSurveyHistoryViewSet, basename='survey') # UserSurveyHistoryViewSet
 
+#  apiCall(`/surveys/${surveyId}/`, { method: 'GET'}) // get survey JSON
 router.register('surveys', SurveyViewSetFrontEnd, basename='survey')
 router.register('available_surveys', AvailableSurveyViewSet, basename='available_survey')
 router.register('survey_history', UserSurveyHistoryViewSet, basename='survey_submission')
